ac01955daff3e59ec02e34cc39bbb8a8d6b5f608
Sh0bra <jshin1@cpp.edu>
2025-01-24 19:45:29 -0800

    added word_count to main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def main():
     path_to_file = "books/frankenstein.txt"
 
+    word_count = 0
     with open(path_to_file) as f:
         file_contents = f.read()
         print(file_contents)
+        words = file_contents.split()
+        for word in words:
+            word_count += 1
+        
+    print(word_count)
+    
 
 main()
