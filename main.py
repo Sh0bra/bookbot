dd812f56a22874f022f60db07213b744285bc8f9
Sh0bra <jshin1@cpp.edu>
2025-01-24 19:48:12 -0800

    created new function for get word count

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 def main():
     path_to_file = "books/frankenstein.txt"
-
-    word_count = 0
+        
     with open(path_to_file) as f:
         file_contents = f.read()
+        word_count = get_word_count(file_contents)
+        # printing file contents and word count
         print(file_contents)
-        words = file_contents.split()
-        for word in words:
-            word_count += 1
-        
-    print(word_count)
+        print(word_count)
     
+def get_word_count(file):
+    words = file.split()
+    word_count = 0
+    for word in words:
+        word_count += 1
+    return word_count
 
 main()
